d94f4387180f15935936019e1e3d543820ebc1a3
omalleya <omalleya@oregonstate.edu>
2017-02-16 09:27:36 -0800

    adds proper formatting of initial prompt to user

--- omalleya.adventure.c
+++ omalleya.adventure.c
@@ -60,6 +60,7 @@ struct Room* getData(char *directory)
     char *fullName = malloc(sizeof(char)*50);
     char *str = malloc(sizeof(char)*200);
     int counter = 0;
+    int connectCount=0;
     char  *temp = malloc(sizeof(char)*200);
 
     struct Room *rooms = malloc(sizeof(struct Room)*7);
@@ -71,6 +72,7 @@ struct Room* getData(char *directory)
         strcat(fullName, ".txt");
         FILE *f = fopen(fullName, "r");
         counter = 0;
+        connectCount = 0;
         while(fgets(str, 100, f)!=0)
         {
             if(counter == 0)
@@ -81,10 +83,10 @@ struct Room* getData(char *directory)
             }else if(str[0] == 'C')
             {
                 printf("%s\n", "connection");
-                rooms[i].connections[counter] = malloc(sizeof(char)*15);
-                strncpy(rooms[i].connections[counter], str+14, 9);
-                printf("\n%s\n", rooms[i].connections[counter]);
-                counter++;
+                rooms[i].connections[connectCount] = malloc(sizeof(char)*15);
+                strncpy(rooms[i].connections[connectCount], str+14, 9);
+                printf("\n%s\n", rooms[i].connections[connectCount]);
+                connectCount++;
             }else {
                 printf("%s\n", "type");
                 strncpy(rooms[i].type, str+11, 11);
@@ -93,9 +95,27 @@ struct Room* getData(char *directory)
             printf("%s", str);
             counter++;
         }
+        while(connectCount < 6)
+        {
+            rooms[i].connections[connectCount] = malloc(sizeof(char)*15);
+            strcpy(rooms[i].connections[connectCount], "");
+            connectCount++;
+        }
         fclose(f);
     }
     return rooms;
+}
+
+void getCurrentRoom()
+{
+
+}
+
+struct Room* setCurrentRoom(struct Room currentRoom, char* nextRoom)
+{
+    struct Room *newRoom = malloc(sizeof(struct Room));
+
+    return newRoom;
 }
 
 int main()
@@ -107,6 +127,7 @@ int main()
     char *dirName = malloc(sizeof(char)*40);
 
     struct Room *rooms;
+    int i=0;
 
     //setting up proper string to access directory
     strcpy(dirName, "./");
@@ -117,14 +138,36 @@ int main()
     rooms = getData(dirName);
 
     printf("\nHELLO::: %s\n", rooms[0].name);
+    printf("\nHELLO::: %s\n", rooms[6].connections[5]);
+
+    struct Room *currentRoom = malloc(sizeof(struct Room));
+    currentRoom = &rooms[0];
 
     do
     {
         printf("%s", next);
-        printf("CURRENT LOCATION: \n");
-        printf("POSSIBLE CONNECTIONS: \n");
+        currentRoom->name[strcspn(currentRoom->name, "\n")] = 0;
+        printf("CURRENT LOCATION: %s\n", currentRoom->name);
+        printf("POSSIBLE CONNECTIONS: ");
+        for(i=0; i<6; i++)
+        {
+            if(strcmp(currentRoom->connections[i], "") != 0)
+            {
+                currentRoom->connections[i][strcspn(currentRoom->connections[i], "\n")] = 0;
+                printf("%s",currentRoom->connections[i]);
+            }
+            
+            if(i+1 < 6 && strcmp(currentRoom->connections[i+1], "") == 0)
+            {
+                printf(".\n");
+                break;
+            }else {
+                printf(", ");
+            }
+        }
         printf("WHERE TO? > ");
         fgets(next, 10, stdin);
+        currentRoom = setCurrentRoom(*currentRoom, next);
     }while(endGame() != 1);
 
     return(0);
